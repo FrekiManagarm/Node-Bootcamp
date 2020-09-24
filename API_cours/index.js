fd91b46a46ef6f32315fa4c8daa5918da4bd6cfb
FrekiManagarm <mathieu.chambaud@ynov.com>
2020-09-24 20:12:34 +0200

    dernier commit

--- API_cours/index.js
+++ API_cours/index.js
@@ -1,7 +1,5 @@
 var express = require('express')
 var app = express()
-
-
 var mysql = require('mysql')
 
 var con = mysql.createConnection({
@@ -11,6 +9,54 @@ var con = mysql.createConnection({
     database: 'exercices'
 })
 
-app.get('/ALL', function(req, res) {
-    
+app.get('/', async function(req, res) {
+    try {
+        const data = await con.query('SELECT * FROM employees')
+        console.log(data)
+        res.json(data)
+    } catch (error) {
+        res.send(error)
+    }
 })
+
+app.post('/users', async function(req, res) {
+    try {
+        const data = await con.query('SELECT first_name, last_name FROM employees')
+        console.log(data)
+        res.json(data)
+    } catch (error) {
+        res.send(error)
+    }
+})
+
+app.get('/users/:id', async function(req, res) {
+    try {
+        const data = con.query('SELECT employee_id, first_name, last_name FROM employee')
+        console.log(data)
+        res.json(data)
+    } catch (error) {
+        res.send(error)
+    }
+})
+
+app.delete('/users/:id', async function(req, res) {
+    try {
+        const id = req.params.id
+        const data = con.query(`DELETE FROM 'employees' WHERE employee_id = ${id};`)
+        console.log(data)
+        res.json(data)
+    } catch (error) {
+        res.send(error)
+    }
+})
+
+app.put('/users/:id', async function(req, res) {
+    try {
+        const id = req.params.id
+        const data = con.query('UPDATE employees')
+    } catch (error) {
+        
+    }
+})
+
+
